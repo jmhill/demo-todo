08ec03f016c79e2168beac8f702662f8b63ec67b
Justin Hill <justin@jmhill.dev>
2025-09-05 22:35:20 -0400

    express healthcheck

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import express from 'express';
+import { healthCheckHandler } from './healthcheck.js';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (reqt, res) => {
-  res.send('Hello, World!');
-});
+app.get('/health', healthCheckHandler);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
+  console.log(`Health check available at http://localhost:${port}/health`);
 });
