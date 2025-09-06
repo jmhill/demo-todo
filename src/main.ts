308d1f3a6a96b6244e5e6cac0803b5ff7b8d361e
Justin Hill <justin@jmhill.dev>
2025-09-05 21:58:31 -0400

    express hello world

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("hello, world!")
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (reqt, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
